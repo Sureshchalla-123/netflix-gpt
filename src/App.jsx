6dcadac49f0cd5e11763c676dc43b6595ba71f9e
Sureshchalla-123 <sureshchalla9876@gmail.com>
2025-04-28 20:52:06 +0530

    fixed issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+import Login from "./components/Login";
+import Browse from "./components/Browse";
+import { createBrowserRouter } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./utils/firebase";
+import { useEffect } from "react";
+import { addUser, removeUser } from "./utils/userSlice";
+import { RouterProvider } from "react-router-dom";
+
 function App() {
-  return <h1 className="text-3xl text-center text-blue-500">Hello world</h1>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(addUser({ uid, email, displayName, photoURL }));
+      } else {
+        dispatch(removeUser());
+      }
+    });
+  });
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "browse",
+      element: <Browse />,
+    },
+  ]);
+
+  return (
+    <RouterProvider router={router}>
+      <Login />
+      <Browse />
+    </RouterProvider>
+  );
 }
 
 export default App;
