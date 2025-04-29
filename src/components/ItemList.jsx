f0734b4e64c9835e1bcc4d962faabe3ea15f63ef
Sureshchalla-123 <sureshchalla9876@gmail.com>
2025-04-29 09:57:04 +0530

    added on hover effect to card

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,7 +3,7 @@ import { POSTER_CDN } from "../utils/constants";
 
 const ItemList = ({ title, data }) => {
   return (
-    <div className="pl-3">
+    <div className="pl-3 ">
       <h1 className="text-white pb-2 text-2xl font-bold">{title}</h1>
       <div className="flex gap-4 overflow-x-scroll scrollbar-hide ">
         {data &&
@@ -12,7 +12,7 @@ const ItemList = ({ title, data }) => {
               src={POSTER_CDN + movie.poster_path}
               alt={movie.title}
               key={movie.id}
-              className="w-[120px] h-[230px] object-cover rounded-lg"
+              className="w-[120px] h-[230px] object-cover rounded-lg transition-transform duration-300 hover:-translate-y-1 "
             />
           ))}
       </div>
