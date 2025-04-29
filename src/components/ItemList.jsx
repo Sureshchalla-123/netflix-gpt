930b0c531c5d1aee042e89367b9069a00e7f12cb
Sureshchalla-123 <sureshchalla9876@gmail.com>
2025-04-29 12:32:04 +0530

    used card item in item list

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import { POSTER_CDN } from "../utils/constants";
+import CardItem from "./CardItem";
 
 const ItemList = ({ title, data }) => {
   return (
     <div className="pl-3 ">
       <h1 className="text-white pb-2 text-2xl font-bold">{title}</h1>
       <div className="flex gap-4 overflow-x-scroll scrollbar-hide ">
-        {data &&
-          data.map((movie) => (
-            <img
-              src={POSTER_CDN + movie.poster_path}
-              alt={movie.title}
-              key={movie.id}
-              className="w-[120px] h-[230px] object-cover rounded-lg transition-transform duration-300 hover:-translate-y-1 "
-            />
-          ))}
+        {data && data.map((movie) => <CardItem data={movie} key={movie.id} />)}
       </div>
     </div>
   );
